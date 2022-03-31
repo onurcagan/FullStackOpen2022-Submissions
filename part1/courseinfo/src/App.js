bdf4bd35841a4b761542c8c627ad482049737c96
Onur Çağan <onur.cagan@geneolab.com>
2022-03-31 17:50:44 +0300

    Commit after 1.3 and 1.4th exercises were complete.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,9 +3,10 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
+  console.log(props)
   return (
     <p>
-      {props.part} {props.exercise}
+      {props.part.name} {props.exercise.exercises}
     </p>
   )
 }
@@ -21,23 +22,29 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  return <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+  return <p>Number of exercises {props.exercises[0].exercises + props.exercises[1].exercises + props.exercises[2].exercises}</p>
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  }
 
   return (
     <div>
       <Header name={course} />
-      <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-      <Total exercises={[exercises1, exercises2, exercises3]} />
+      <Content part={[part1, part2, part3]} exercises={[part1, part2, part3]} />
+      <Total exercises={[part1, part2, part3]} />
     </div>
   )
 }
