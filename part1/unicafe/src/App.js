f23e609b0c2d04c95b6754e20f56078fa2678f8e
Onur Çağan <onurcagann@gmail.com>
2022-04-02 02:44:22 +0300

    unicafe: Exercise 1.6 completed.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,49 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>
+}
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const setToValue = (newGoodValue, newNeutralValue, newBadValue) => () => {
+    if (good !== newGoodValue) {
+      setGood(newGoodValue)
+    }
+
+    if (neutral !== newNeutralValue) {
+      setNeutral(newNeutralValue)
+    }
+
+    if (bad !== newBadValue) {
+      setBad(newBadValue)
+    }
+  }
 
   return (
-    <>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={decreaseByOne} text="minus" />
-      <Button onClick={setToZero} text="zero" />
-    </>
+    <div>
+      <h1>
+        <strong> Give Feedback </strong>
+      </h1>
+      <Button onClick={setToValue(good + 1, neutral, bad)} text="good" />
+      <Button onClick={setToValue(good, neutral + 1, bad)} text="neutral" />
+      <Button onClick={setToValue(good, neutral, bad + 1)} text="bad" />
+      <br />
+
+      <h1>
+        <strong>Statistics</strong>
+      </h1>
+      <p>Good Feedback amount is - {good}</p>
+      <br />
+      <p>Neutral Feedback amount is - {neutral}</p>
+      <br />
+      <p>Bad Feedback amount is - {bad}</p>
+    </div>
   )
 }
+
 export default App
