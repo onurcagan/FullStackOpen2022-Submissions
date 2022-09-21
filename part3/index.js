b36679488c493f3cd2db2036d69594df337e19c5
Onur Çağan <onurcagann@gmail.com>
2022-09-21 04:09:09 +0300

    part3: exercises 3.13, 3.14, 3.15, 3.17, 3.18 complete.

--- part3/index.js
+++ part3/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const { json } = require('express')
 const express = require('express')
 const morgan = require('morgan')
@@ -6,80 +7,83 @@ const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
 const baseUrl = '/api/persons'
+const Person = require('./models/person')
 
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
-let phonebook = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
+// let phonebook = [
+//   {
+//     id: 1,
+//     name: 'Arto Hellas',
+//     number: '040-123456',
+//   },
+//   {
+//     id: 2,
+//     name: 'Ada Lovelace',
+//     number: '39-44-5323523',
+//   },
+//   {
+//     id: 3,
+//     name: 'Dan Abramov',
+//     number: '12-43-234345',
+//   },
+//   {
+//     id: 4,
+//     name: 'Mary Poppendieck',
+//     number: '39-23-6423122',
+//   },
+// ]
+
 morgan.token('content', (request) => request.method === 'POST' && request.body.name && JSON.stringify(request.body))
 
 app.get('/info', (request, response) => {
-  response.send(`
-  <div>Phonebook has info for ${phonebook.length} people.</div> 
-  <div>${new Date()}</div>`)
-})
-
-app.get('/', (request, response) => {
-  response.send("<h1>What's up chump?</h1>")
+  const count = Person.count({}, (error, count) =>
+    response.status(200).send(`
+  <div>Phonebook has info for ${count} people.</div> 
+  <div>Request date: ${new Date()}</div>`),
+  ).catch((err) => `Some error happened, here's the message: ${err.message}`)
 })
 
 app.get(baseUrl, (request, response) => {
-  response.json(phonebook)
+  Person.find({}).then((persons) => {
+    response.status(200).json(persons)
+  })
 })
 
 app.get(`${baseUrl}/:id`, (request, response) => {
-  const id = Number(request.params.id)
-  const person = phonebook.find((person) => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).send("The person you're looking for is non-existent.")
-  }
+  Person.findById(request.params.id)
+    .then((person) => {
+      response.json(person)
+    })
+    .catch((error) =>
+      response.status(404).send(`Couldn't fetch the contact with the given ID, here's the error: ${error.message}`),
+    )
 })
 
 app.put(`${baseUrl}/:id`, (request, response) => {
-  const id = Number(request.params.id)
-  if (phonebook.some((person) => person.id === id)) {
-    const updatedPerson = request.body
-    phonebook.splice(id - 1, 1, updatedPerson)
-    return response.send(updatedPerson)
-  }
-  response.status(404).send('This person was deleted from the contacts list.')
+  Person.findByIdAndUpdate(
+    request.params.id,
+    {
+      number: request.body.number,
+    },
+    (err, person) => {
+      if (err) {
+        response.status(404).send(`Couldn't update the contact with the given ID, here's the error: ${err.message}`)
+      } else response.status(204).send(`Updated user: ${person.name}`)
+    },
+  )
 })
 
 app.delete(`${baseUrl}/:id`, (request, response) => {
-  const id = Number(request.params.id)
-  phonebook = phonebook.filter((person) => person.id !== id)
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(response.status(204).end())
+    .catch((error) =>
+      response.status(404).send(`Couldn't delete the contact with the given ID, here's the error: ${error.message}`),
+    )
 })
-
-const generateId = () => {
-  const maxId = phonebook.length > 0 ? Math.max(...phonebook.map((n) => n.id)) : 0
-  return maxId + 1
-}
 
 app.post(baseUrl, (request, response) => {
   const body = request.body
@@ -89,19 +93,23 @@ app.post(baseUrl, (request, response) => {
     })
   }
 
-  if (phonebook.find((person) => person.name === body.name)) {
-    return response.status(400).json({ error: 'name must be unique' })
-  }
+  // if (
+  //   Person.find((persons) => {
+  //     console.log(persons)
+  //     persons.map((person) => person.name === body.name)
+  //   })
+  // ) {
+  //   return response.status(400).json({ error: 'name must be unique' })
+  // }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
+  })
 
-  phonebook = phonebook.concat(person)
-
-  response.json(person)
+  person.save().then((savedPerson) => {
+    response.json(savedPerson)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
@@ -110,7 +118,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
